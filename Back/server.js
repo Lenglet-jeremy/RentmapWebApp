a0e853672387b514cfb19dc92c5e4fc66b4182b7
Lenglet-jeremy <lengletjeremy.pro@gmail.com>
2025-04-13 11:21:36 +0200

    Test Deploiement 5

--- Back/server.js
+++ Back/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const fs = require('fs');
 const path = require('path');
-import fetch from 'node-fetch';
+const fetch = require('node-fetch');
 require('dotenv').config();
 
 const PORT = process.env.PORT || 5000;
